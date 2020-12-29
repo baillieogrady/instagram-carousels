d3b1c02ceb77605ea7b707940ad351dbe7340196
Baillie O'Grady <baillieogrady@Baillies-MacBook-Pro.local>
2020-12-29 11:42:06 +0000

    add image zoom carousel and master template

---
+++ 031-fancy-burger/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="#" class="header__icon">
+        <div class="header__burger">
+          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
+          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
+          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default App;
