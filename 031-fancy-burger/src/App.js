c9803d1a42a36031b1adae28b0afa93ea16035db
Baillie O'Grady <baillieogrady@Baillies-MacBook-Pro.local>
2020-12-29 16:12:27 +0000

    add

--- 031-fancy-burger/src/App.js
+++ 031-fancy-burger/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <a href="#" class="header__icon">
-        <div class="header__burger">
-          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
-          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
-          <span class="header__line w-brand-3 lg:w-4 xl:w-6 h-brand-1 block bg-hos-black"></span>
+      <a href="#" class="header__icon z-50 w-brand-20 h-brand-20 flex justify-center items-center">
+      <div class="relative h-full w-full">
+        <span class="header__border"></span>
+        <span class="header__border"></span>
+        <span class="header__border"></span>
+        <span class="header__border"></span>
+        <div class="header__burger absolute left-brand-1/2 top-brand-1/2 transform -translate-x-1/2 -translate-y-1/2 z-40">
+          <span class="header__line w-brand-3 lg:w-4 xl:w-5 h-brand-1 block"></span>
+          <span class="header__line w-brand-3 lg:w-4 xl:w-5 h-brand-1 block"></span>
+          <span class="header__line w-brand-3 lg:w-4 xl:w-5 h-brand-1 block"></span>
         </div>
-      </a>
+      </div>
+    </a>
     </div>
   );
 }
--- 031-fancy-burger/tailwind.config.js
+++ 031-fancy-burger/tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'brand-black': '#19232C',
+      },
+      width: {
+        'brand-3': '.8125rem',
+        'brand-20': '5.375rem',
+      },
+      height: {
+        'brand-1': '1px',
+        'brand-20': '5.375rem',
+      },
+      inset: {
+        'brand-1/2': '50%'
+      }
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
